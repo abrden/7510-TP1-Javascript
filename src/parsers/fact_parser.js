7605fcf655e118d239ea280b13584f1cd9c86fdc
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-09-29 17:44:52 -0300

    Add rule parser tests

--- src/parsers/fact_parser.js
+++ src/parsers/fact_parser.js
@@ -1,7 +1,7 @@
 var Fact = require('../entities/fact');
 
 var FactParser = function (factStr) {
-  var factArr = factStr.split(/,\s*|\(|\)\.|\)/).slice(0, -1);
+  var factArr = factStr.split(/,\s*|\(|\)\.|\)/).filter(Boolean);
   return new Fact(factArr[0], factArr.slice(1))
 };
 
--- src/parsers/rule_parser.js
+++ src/parsers/rule_parser.js
@@ -1,7 +1,8 @@
 var FactParser = require('./fact_parser');
+var Rule = require('../entities/rule');
 
 var RuleParser =  function (ruleStr) {
-  var ruleArr = ruleStr.split(/\s*:-\s*|\)\s*,\s*|\./).slice(0, -1).map(FactParser.parse);
+  var ruleArr = ruleStr.split(/\s*:-\s*|\)\s*,\s*|\./).slice(0, -1).map(FactParser);
   return new Rule(ruleArr[0], ruleArr.slice(1));
 };
 
